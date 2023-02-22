727d1a8cbeecee549d1e7e3bf385d756d5f49ef0
anboo <developer@rusdteam.ru>
2023-02-22 09:14:20 +0300

    Revert "telegram_id index"
    This reverts commit 6185712cd57b6aa54483f146403e8de1b8025536.

--- migrations/20230222091217_create_index_users_telegram_id.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-CREATE INDEX users_telegram_id ON users (telegram_id);
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-DROP INDEX users_telegram_id;
--- +goose StatementEnd
